e8b9b3285dcf942abeb5d8a3429bdbc46582747d
taki-21 <taki.it.0208.21@gmail.com>
2022-03-22 23:02:55 +0900

    add: 関数の追加

--- sample_wrapper/file_operation.py
+++ sample_wrapper/file_operation.py
@@ -6,7 +6,7 @@ from typing import List
 
 def read_input_file_paths(
         input_root_path: pathlib.PosixPath,
-        extension: str) -> List:
+        extension: str):
     """最下層に存在する指定した拡張子のファイルを全て取得する
     Args:
         input_root_path (pathlib.PosixPath): ディレクトリのパス
@@ -17,3 +17,25 @@ def read_input_file_paths(
     """
     all_path_list = list(input_root_path.glob(f'**/*.{extension}'))
     return all_path_list
+
+
+def generate_output_name_with_work_id(
+        work_id: int,
+        file_stem: str,
+        extension: str = ''):
+    """IDを付与したファイル名(or ディレクトリ名) を生成する
+       ディレクトリ名は生成するときはextensionは指定しない
+    Args:
+        work_id (int): ワークID
+        file_stem (str): 拡張子なしのファイル名
+        extension (str, optional): ファイルに付与したい拡張子. Defaults to ''. (pdf, json, xlsx ...)
+
+    Returns:
+        output_name (str): IDを付与したファイル名(or ディレクトリ名)
+    """
+    work_id = str(work_id)
+    if extension:
+        output_name = f"[#{work_id}]_{file_stem}.{extension}"
+    else:
+        output_name = f"[#{work_id}]_{file_stem}"
+    return output_name

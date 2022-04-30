5e34e8eb920d6be435ad955fcb5519c0c784a141
taki-21 <taki.it.0208.21@gmail.com>
2022-04-30 19:03:25 +0900

    Clean: 関数の追加

---
+++ samplewrapper/b/abc.py
@@ -0,0 +1,12 @@
+def calculate_two_number(a, b):
+    """二つの数字の和を計算する
+
+    Args:
+        a (int): 数字_1
+        b (int): 数字_2
+
+    Returns:
+        int: 合計
+    """
+    sm = a + b
+    return sm

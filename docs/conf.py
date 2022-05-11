36b5f6f883762798e8b977875f8b4f041e01c017
taki-21 <taki.it.0208.21@gmail.com>
2022-05-11 23:16:10 +0900

    add: conf.pyの編集

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-import sphinx_rtd_theme
+
 # sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../pythonwrapper/a'))
 sys.path.insert(0, os.path.abspath('../pythonwrapper/b'))
@@ -51,8 +51,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = ["_themes", sphinx_rtd_theme.get_html_theme_path()]
+html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
